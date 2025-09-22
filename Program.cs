8d3cc5cd37996a78315a7fc6ca6ee56b679b0d13
NguyenPhamQuocHuy-20130283 <100356312+NguyenPhamQuocHuy-20130283@users.noreply.github.com>
2025-09-22 15:37:46 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 builder.WebHost.UseUrls($"http://*:{port}");
 // Add services to the container.
 builder.Services.AddControllers()
